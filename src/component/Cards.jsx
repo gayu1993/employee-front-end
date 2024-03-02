39374d075a4b50a0f23b3b6ece7a7ecca3bd6c2c
vickneswari-s-t <vickneswari.s.t@gmail.com>
2024-03-02 14:59:26 +0530

    changes updated

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -1,37 +1,49 @@
 
-import React from 'react'
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+// In your main JavaScript file (e.g., index.js or App.jsx)
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Cards () {
-  return  <div className='container'>
-    <div className="row g-4">
-    <div className="col">
-    <div className="card h-100">
-    <img src="src/assets/companylogo.png" className="card-img-top" alt="..."/>
-    <div className="card-body">
-    <Button variant="primary">Primary</Button>{' '}
-      </div>
+function Cards() {
+  return (
+    <div className='container'>
+      <div className="row g-4">
+        <div className="col-md-4">
+          <div className="card h-100">
+            <img src="src/assets/employee details.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <Button variant="primary">Employee Details</Button>{' '}
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card h-100">
+            <img src="src/assets/project details.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+            <Button variant="primary">Project details</Button>{' '}
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card h-100">
+            <img src="src/assets/performance report.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+            <Button variant="primary">Performance report</Button>{' '}
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card h-100">
+            <img src="src/assets/my info.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+            <Button variant="primary">My info</Button>{' '}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div className="col">
-    <div className="card h-100">
-    <img src="src/assets/companylogo.png" className="card-img-top" alt="..."/>
-    <div className="card-body">
-   <a href="" class="btn">Click Here</a>
-       
-      </div>
-      </div>
-    </div>
-  </div>
-
-  </div>
+  );
 }
 
-export default Cards
-  
-
-
-  
-
-
+export default Cards;
